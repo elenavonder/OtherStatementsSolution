6f4d1fac32d275fe25af08773d68a4fc1208eb21
Elena Vonder Meulen <evonder13@gmail.com>
2020-09-28 11:21:21 -0400

    Add project files.

---
+++ OtherStatementsProject/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OtherStatementsProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //Continue Statement only looking at odd numbers
+            for (var idx = 1; idx <= 10; idx++)
+            {
+                if (idx % 2 == 0)
+                {
+                    continue;
+                }
+                Console.WriteLine($"{idx} is odd! ");
+            }
+         //Ternary Operater
+            var TodayIsMonday = false; //If true will say Monday
+            var DayOfTheWeek = "";
+            DayOfTheWeek = (TodayIsMonday == true)//can do with or without == true
+                ? "Monday" : "Not Monday";
+            Console.WriteLine(DayOfTheWeek);
+        }
+    }
+}
